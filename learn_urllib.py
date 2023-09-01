0d0e9751e9fb343733d347e94cf7f770ab3ff9bf
xyh <975573743@qq.com>
2023-09-01 16:22:23 +0800

    1

--- learn_urllib.py
+++ learn_urllib.py
@@ -4,6 +4,7 @@ import urllib.parse
 url1 = 'https://www.baidu.com/s?wd='
 
 # UA反爬虫，需要对request进行定制
+# 有的需要cookie
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 '
                   'Safari/537.36 Edg/116.0.1938.62'
@@ -11,11 +12,17 @@ headers = {
 # 汉字转化
 name = urllib.parse.quote('周杰伦')
 url = url1 + name
+data = {
+    'name': '周杰伦',
+    'sex': '男'
+}
+data = urllib.parse.urlencode(data).encode('utf-8')
+# post请求参数要放到data中，并且要指定编码类型
 request = urllib.request.Request(url, headers=headers)
 
 response = urllib.request.urlopen(request)
 content = response.read().decode('utf-8')
-print(content)
+# print(content)
 
 
 # urllib.request.urlretrieve(url,'baidu,html')
